e36a1ec317d40645fdcd62cf8608c6d79bc1d49b
markuz449 <markuz449@gmail.com>
2021-01-13 13:31:10 +1300

    Opening a file and printing contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Testing file");
+    let file = open_file();
+    print_file(file);
 }
+
+fn open_file() -> std::string::String {
+    let file = fs::read_to_string("test.txt").expect("Error opening file");
+    file
+}
+
+fn print_file(file: std::string::String){
+    println!("{}", file);
+}
+
