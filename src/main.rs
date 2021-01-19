74bcd9c5905b319a99a361e57022462349f5eba9
markuz449 <markuz449@gmail.com>
2021-01-19 14:20:41 +1300

    Cleaned the text and saving bracketed information

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,8 @@ mod text_manipulation;
 fn main() {
     let filename: String = String::from("Story/[C0,a].txt");
     let file_text = file_handler::open_file(filename);
-    print_file(&file_text);
 
-    println!("Bracket Strings: {:?}", text_manipulation::get_bracket_strings(&file_text));
-}
-
-fn print_file(file: &String) {
-    println!("{}", file);
+    println!("Bracket Strings: {:?}", text_manipulation::get_bracket_strings(file_text));
 }
 
 fn print_type_of<T>(_: &T) {
--- src/text_manipulation.rs
+++ src/text_manipulation.rs
@@ -1,14 +1,26 @@
 use regex::Regex;
 
-pub fn get_bracket_strings(text: &str){
-    let bracket_num: usize = bracket_count(text);
-    let start: usize = find_indicies(text, '[');
-    let end: usize = find_indicies(text, ']');
+pub fn get_bracket_strings(mut text: String) -> Vec<String>{
+    let bracket_num: usize = bracket_count(&text);
+    let mut bracket_strings: Vec<String> = Vec::new();
+    let mut start: usize;
+    let mut end: usize;
 
-    println!("Bracket Num: {}, Start Index: {:?}, End Index: {:?}", bracket_num,  start, end);
+    for _ in 0..bracket_num{
+        start = find_indicies(&text, '[');
+        end = find_indicies(&text, ']');
 
-    let slice: &str = &text[start..(end + 1)];
-    println!("Slice: {}", slice);
+        println!("Bracket Num: {}, Start Index: {:?}, End Index: {:?}", bracket_num,  start, end);
+
+        bracket_strings.push(get_slice(&text, start, end));
+        println!("Slices: {:?}", bracket_strings);
+
+        println!("Removing Brackets...");
+        text = remove_brackets(text, start, end);
+    }
+
+    println!("Clean Text: {}", text);
+    bracket_strings
 }
 
 fn bracket_count(text: &str) -> usize {
@@ -31,4 +43,24 @@ fn find_indicies(text: &str, find_char: char) -> usize{
         std::process::exit(0);
     }
     return_index
+}
+
+fn get_slice(text: &str, start: usize, end: usize) -> String{
+    let mut clean_text: String = String::from(text);
+    let slice_option = clean_text.get_mut(start..(end + 1));
+    let mut slice: String = String::from("");
+    match slice_option{
+        Some(x) => slice = x.to_string(),
+        None => println!("There is no slice"),
+    }
+    if slice.eq(""){
+        println!("Exiting Program...");
+        std::process::exit(0);
+    }
+    slice
+}
+
+fn remove_brackets(mut text: String, start: usize, end: usize) -> String{
+    text.replace_range(start..(end + 1), "");
+    text
 }
