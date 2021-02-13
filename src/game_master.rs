53e514229a798d0d0ff42411efcbe9bc4a58f740
markuz449 <markuz449@gmail.com>
2021-02-13 13:16:02 +1300

    Now writing everything to stdout and hideing the cursor

--- src/game_master.rs
+++ src/game_master.rs
@@ -1,9 +1,9 @@
 use std::io::*;
 use colored::*;
-use termion::{event::Key};
+use termion::{event::Key, raw::RawTerminal};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::cursor::HideCursor;
+use termion::cursor::Hide;
 
 use crate::story_page::StoryPage;
 use crate::file_handler;
@@ -29,9 +29,11 @@ pub struct Character{
     pub is_girl: bool,
 }
 
+type Out = RawTerminal<Stdout>;
+
 pub fn game_loop() {
     let stdin = stdin();
-    let mut stdout = HideCursor::from(stdout().into_raw_mode().unwrap());
+    let mut stdout: Out = stdout().into_raw_mode().unwrap();
     let mut help_active: bool = false;
     let mut character_creator_active: bool = false;
 
@@ -62,7 +64,7 @@ pub fn game_loop() {
     let mut game_state: GameState = GameState{story_path, re_read_mode, previous_story_num, current_story_point, 
         planet, title, title_active, terminal_width};
     
-    print_title(&game_state);
+    stdout = write_title(&game_state, stdout);
     //writeln!(stdout, "Hello").unwrap();
 
     // Detecting keydown events
@@ -74,35 +76,35 @@ pub fn game_loop() {
                 Key::Esc => break,
                 Key::Left => {
                     character.is_girl = false;
-                    print_character_creator(&character, &game_state);
+                    stdout = write_character_creator(&character, &game_state, stdout);
                 },
                 Key::Right => {
                     character.is_girl = true;
-                    print_character_creator(&character, &game_state);
+                    stdout = write_character_creator(&character, &game_state, stdout);
                 },
                 Key::Char('\n') => {
                     if character.name.len() == 0{
-                        print_character_creator(&character, &game_state);
+                        stdout = write_character_creator(&character, &game_state, stdout);
                     } else {
                         character_creator_active = false;
                         filename = String::from("Story/[C0].txt");
                         file_text = file_handler::open_text_file(filename, terminal_width);
                         story = StoryPage::initial_story_page(file_text, &character);
-                        print_story(&story, &game_state);
+                        stdout = write_story(&story, &game_state, stdout);
                     }
                 },
                 Key::Backspace => {
                     let name_length: usize = character.name.len();
                     if name_length > 0{
                         character.name.truncate(character.name.len() - 1);
-                        print_character_creator(&character, &game_state);
+                        stdout = write_character_creator(&character, &game_state, stdout);
                     }
                 },
                 Key::Char(c) => {
                     let name_length: usize = character.name.len();
                     if name_length < 20{
                         character.name.push(c);
-                        print_character_creator(&character, &game_state);
+                        stdout = write_character_creator(&character, &game_state, stdout);
                     }
                 },
                 _ => {},
@@ -112,7 +114,7 @@ pub fn game_loop() {
             match c.unwrap() {
                 _ => ({
                     help_active = false;
-                    print_story(&story, &game_state);
+                    stdout = write_story(&story, &game_state, stdout);
                 }),
             }
         } else {
@@ -120,31 +122,37 @@ pub fn game_loop() {
             match c.unwrap() {
                 Key::Ctrl('c') => break,
                 Key::Esc => {
-                    print!("\x1bc");
+                    write!(stdout, "{}", "\x1bc").unwrap();
                     break
                 },
                 Key::Char('h') => {
-                    if !title_active{
+                    if !game_state.title_active{
                         help_active = true;
-                        help(&game_state);
+                        stdout = help(&game_state, stdout);
                     }
                 },
                 Key::Char('r') => {
                     game_state.title_active = true;
                     game_state.story_path.clear();
-                    print_title(&game_state);
+                    stdout = write_title(&game_state, stdout);
                 },
                 Key::Up => {
-                    story = change_option(story, -1, &game_state);
+                    if !game_state.title_active && !story.game_over{
+                        story = story.change_selected_option(-1);
+                        stdout = write_story(&story, &game_state, stdout);
+                    }
                 },
                 Key::Down => {
-                    story = change_option(story, 1, &game_state);
+                    if !game_state.title_active && !story.game_over{
+                        story = story.change_selected_option(1);
+                        stdout = write_story(&story, &game_state, stdout);
+                    }
                 },
                 Key::Left => {
                     game_state = re_read(&story, game_state, true);
                     if game_state.re_read_mode {
                         story = open_previous_story(story, &game_state);
-                        print_story(&story, &game_state);
+                        stdout = write_story(&story, &game_state, stdout);
                     }
                 },
                 Key::Right => {
@@ -152,19 +160,19 @@ pub fn game_loop() {
                     if re_read_mode {
                         story = open_previous_story(story, &game_state);
                     }
-                    print_story(&story, &game_state);
+                    stdout = write_story(&story, &game_state, stdout);
                 },
                 Key::Char('\n') => {
                     if game_state.title_active{
                         game_state.title_active = false;
                         character_creator_active = true;
-                        print_character_creator(&character, &game_state);
+                        stdout = write_character_creator(&character, &game_state, stdout);
                     } else if story.game_over || game_state.re_read_mode{
                         continue;
                     } else{
                         game_state = update_story_path(&story, game_state);
                         story = submit_option(story, &game_state);
-                        print_story(&story, &game_state);
+                        stdout = write_story(&story, &game_state, stdout);
                     }
                 },
                 _ => (),
@@ -174,42 +182,46 @@ pub fn game_loop() {
     }
 }
 
-// Prints the title sequence and the planet
-fn print_title(game_state: &GameState) {
-    print!("{}", "\x1bc");
-    println!("{}", format!("{:^1$}", game_state.planet.bold().blue(), game_state.terminal_width));
-    println!("{}", format!("{:^1$}", game_state.title.bold().red(), game_state.terminal_width));
+// Writes the title sequence and the planet to stdout
+fn write_title(game_state: &GameState, mut stdout: Out) -> Out{
+    write!(stdout, "{}", "\x1bc").unwrap();
+    writeln!(stdout, "{}", format!("{:^1$}", game_state.planet.bold().blue(), game_state.terminal_width)).unwrap();
+    writeln!(stdout, "{}", format!("{:^1$}", game_state.title.bold().red(), game_state.terminal_width)).unwrap();
     let start_message: String = format!("{} {} {}", "Press".bold().italic(), "Enter".bold().italic().green(), "to Start".bold().italic());
-    println!("\r{}\r", format!("{:^1$}", start_message, game_state.terminal_width + (start_message.len() / 2)));
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", start_message, game_state.terminal_width + (start_message.len() / 2))).unwrap();
+    writeln!(stdout, "{}", Hide).unwrap();
+    stdout
 }
 
-// Printing the story to terminal
-fn print_story(story: &StoryPage, game_state: &GameState){
-    print!("{}", "\x1bc");
-    println!("\r\n{}\r\n", format!("{:^1$}", "Devolution".bold().green(), game_state.terminal_width));
-    println!("{}", story.text.italic());
+// Writes the story page to stdout
+fn write_story(story: &StoryPage, game_state: &GameState, mut stdout: Out) -> Out{
+    write!(stdout, "{}", "\x1bc").unwrap();
+    writeln!(stdout, "\r\n{}\r\n", format!("{:^1$}", "Devolution".bold().green(), game_state.terminal_width)).unwrap();
+    writeln!(stdout, "{}", story.text.italic()).unwrap();
 
     if !story.game_over && !game_state.re_read_mode {
-        println!("\r{}\r", format!("{:^1$}", "Choices".bold().italic().yellow(), game_state.terminal_width));
+        writeln!(stdout, "\r{}\r", format!("{:^1$}", "Choices".bold().italic().yellow(), game_state.terminal_width)).unwrap();
         for i in 0..story.option_text.len(){
             if i == story.selection_num{
-                print!("\r{}\r", story.option_text[i].blue().bold());
+                write!(stdout, "\r{}\r", story.option_text[i].blue().bold()).unwrap();
             } else{
-                print!("\r{}\r", story.option_text[i].bold());
+                write!(stdout, "\r{}\r", story.option_text[i].bold()).unwrap();
             }
         }
     } else if game_state.re_read_mode {
-        println!("\r{}\r", format!("{:^1$}", "Your Choice".bold().italic().yellow(), game_state.terminal_width));
+        writeln!(stdout, "\r{}\r", format!("{:^1$}", "Your Choice".bold().italic().yellow(), game_state.terminal_width)).unwrap();
         let choice_num: usize = game_state.story_path[game_state.previous_story_num].choice_num;
-        print!("\r{}\r", story.option_text[choice_num].red().bold());
+        write!(stdout, "\r{}\r", story.option_text[choice_num].red().bold()).unwrap();
     }
 
-    println!("\r\n{}\r", format!("{:^1$}", "To Quit, Press 'Esc'. For Help, Press 'h'".bold().italic().green(), game_state.terminal_width));
-    print_story_status(&story, &game_state);
+    writeln!(stdout, "\r\n{}\r", format!("{:^1$}", "To Quit, Press 'Esc'. For Help, Press 'h'".bold().italic().green(), game_state.terminal_width)).unwrap();
+    writeln!(stdout, "{}", Hide).unwrap();
+    //print_story_status(&story, &game_state);
+    stdout
 }
 
-// Prints the Help menu for the game
-fn help(game_state: &GameState){
+// Writes the Help menu for the game
+fn help(game_state: &GameState, mut stdout: Out) -> Out{
     let width: usize = game_state.terminal_width;
     let title: ColoredString =            " Help Menu ".bold().green();
     let controls: ColoredString =         "Controls:".bold().yellow();
@@ -223,33 +235,35 @@ fn help(game_state: &GameState){
     let exit: ColoredString =             "  Press Any Key to return to the game  ".bold().green();
 
     // Printing the help screen
-    print!("\r{}\r", "\x1bc");
-    println!();
-    println!();
-    println!();
-    println!("\r{}\r", format!("{:^1$}", title, width));
-    println!();
-    println!("\r{}\r", format!("{:^1$}", "Welcome to Devolution, a sci-fi adventure where you choose".italic(), width));
-    println!("\r{}\r", format!("{:^1$}", "how you progresses!".italic(), width));
-    println!();
-    println!();
-    println!("\r{}\r", format!("{:^1$}", controls, width));
-    println!();
-    println!("\r{}\r", format!("{:>1$}", header, (width / 5) + header.len()));
-    println!("\r{}\r", format!("{:>1$}", enter_control, (width / 5) + enter_control.len()));
-    println!("\r{}\r", format!("{:>1$}", up_down_control, (width / 5) + up_down_control.len()));
-    println!("\r{}\r", format!("{:>1$}", re_read_control, (width / 5) + re_read_control.len()));
-    println!("\r{}\r", format!("{:>1$}", quit_control, (width / 5) + quit_control.len()));
-    println!("\r{}\r", format!("{:>1$}", help_control, (width / 5) + help_control.len()));
-    println!("\r{}\r", format!("{:>1$}", reset_control, (width / 5) + reset_control.len()));
-    println!();
-    println!();
-    println!("\r{}\r", format!("{:^1$}", exit, width));
-    print!("\r");
+    write!(stdout, "\r{}\r", "\x1bc").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", title, width)).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", "Welcome to Devolution, a sci-fi adventure where you choose".italic(), width)).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", "how you progresses!".italic(), width)).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", controls, width)).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:>1$}", header, (width / 5) + header.len())).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:>1$}", enter_control, (width / 5) + enter_control.len())).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:>1$}", up_down_control, (width / 5) + up_down_control.len())).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:>1$}", re_read_control, (width / 5) + re_read_control.len())).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:>1$}", quit_control, (width / 5) + quit_control.len())).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:>1$}", help_control, (width / 5) + help_control.len())).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:>1$}", reset_control, (width / 5) + reset_control.len())).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", exit, width)).unwrap();
+    writeln!(stdout, "\r").unwrap();
+    writeln!(stdout, "{}", Hide).unwrap();
+    stdout
 }
 
-// Prints the Character creator
-fn print_character_creator(character: &Character, game_state: &GameState) {
+// Writes the Character creator
+fn write_character_creator(character: &Character, game_state: &GameState, mut stdout: Out) -> Out {
     let width: usize = game_state.terminal_width;
     let no_name: bool = character.name.len() == 0;
     let name: ColoredString = character.name.bold().blue();
@@ -271,39 +285,32 @@ fn print_character_creator(character: &Character, game_state: &GameState) {
     let confirm: ColoredString = "Press 'Enter' to continue with your character".bold().green();
 
     // Printing the character creator screen
-    print!("\r{}\r", "\x1bc");
-    println!();
-    println!();
-    println!();
-    println!("\r{}\r", format!("{:^1$}", title, width));
-    println!();
-    println!("\r{}\r", format!("{:^1$}", name_title, width));
-    println!("\r{}\r", format!("{:^1$}", top_box, width));
-    println!("\r{}\r", format!("{:^1$}", name_box.bold(), width));
-    println!("\r{}\r", format!("{:^1$}", bottom_box, width));
+    write!(stdout, "\r{}\r", "\x1bc").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", title, width)).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", name_title, width)).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", top_box, width)).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", name_box.bold(), width)).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", bottom_box, width)).unwrap();
     if no_name{
-        println!("\r{}\r", format!("{:^1$}", name_error, width));
-        println!();
+        writeln!(stdout, "\r{}\r", format!("{:^1$}", name_error, width)).unwrap();
+        writeln!(stdout, "").unwrap();
     } else{
-        println!();
-        println!();
+        writeln!(stdout, "").unwrap();
+        writeln!(stdout, "").unwrap();
     }
-    println!();
-    println!("\r{}\r", format!("{:^1$}", gender_title, width));
-    println!();
-    println!("\r{}\r", format!("{:^1$}", gender_option, width + (gender_option.len() / 2)));
-    println!();
-    println!("\r{}\r", format!("{:^1$}", confirm, width));
-    println!();
-}
-
-// Changes current option and reprints story
-fn change_option(mut story: StoryPage, change: i8, game_state: &GameState) -> StoryPage{
-    if !game_state.title_active && !story.game_over{
-        story = story.change_selected_option(change);
-        print_story(&story, &game_state);
-    }
-    story
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", gender_title, width)).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", gender_option, width + (gender_option.len() / 2))).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", confirm, width)).unwrap();
+    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "{}", Hide).unwrap();
+    stdout
 }
 
 // Updates the users story path
