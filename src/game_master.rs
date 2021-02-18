bd11148db95ca277626be51d14b3522bd78f99a6
markuz449 <markuz449@gmail.com>
2021-02-18 13:37:01 +1300

    All writing is now done using termion instead of the colored crate

--- src/game_master.rs
+++ src/game_master.rs
@@ -1,5 +1,4 @@
 use std::io::*;
-use colored::*;
 use termion::{event::Key, raw::RawTerminal};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
@@ -199,10 +198,11 @@ pub fn game_loop() {
 
 // Writes the title sequence and the planet to stdout
 fn write_title(game_state: &GameState, mut stdout: Out) -> Out{
-    write!(stdout, "{}", "\x1bc").unwrap();
-    writeln!(stdout, "{}", format!("{:^1$}", game_state.planet.bold().blue(), game_state.terminal_width)).unwrap();
-    writeln!(stdout, "{}", format!("{:^1$}", game_state.title.bold().red(), game_state.terminal_width)).unwrap();
-    let start_message: String = format!("{} {} {}", "Press".bold().italic(), "Enter".bold().italic().green(), "to Start".bold().italic());
+    write!(stdout, "{}{}", "\x1bc", style::Bold).unwrap();
+    writeln!(stdout, "{}{}", color::Fg(color::Blue), format!("{:^1$}", game_state.planet, game_state.terminal_width)).unwrap();
+    writeln!(stdout, "{}{}", color::Fg(color::Red), format!("{:^1$}", game_state.title, game_state.terminal_width)).unwrap();
+    writeln!(stdout, "{}{}", color::Fg(color::LightWhite), style::Italic).unwrap();
+    let start_message: String = format!("{} {}{}{} {}", "Press", color::Fg(color::Green), "Enter", color::Fg(color::LightWhite), "to Start");
     writeln!(stdout, "\r{}\r", format!("{:^1$}", start_message, game_state.terminal_width + (start_message.len() / 2))).unwrap();
     writeln!(stdout, "{}", Hide).unwrap();
     stdout
@@ -210,26 +210,27 @@ fn write_title(game_state: &GameState, mut stdout: Out) -> Out{
 
 // Writes the story page to stdout
 fn write_story(story: &StoryPage, game_state: &GameState, mut stdout: Out) -> Out{
-    write!(stdout, "{}", "\x1bc").unwrap();
-    writeln!(stdout, "\r\n{}\r\n", format!("{:^1$}", "Devolution".bold().green(), game_state.terminal_width)).unwrap();
-    writeln!(stdout, "{}", story.text.italic()).unwrap();
+    write!(stdout, "{}{}", "\x1bc", style::Bold).unwrap();
+    writeln!(stdout, "\r\n{}{}\r\n", color::Fg(color::Green), format!("{:^1$}", "Devolution", game_state.terminal_width)).unwrap();
+    writeln!(stdout, "{}{}{}", color::Fg(color::White), style::Italic, story.text).unwrap();
+    writeln!(stdout, "{}", style::NoItalic).unwrap();
 
     if !story.game_over && !game_state.re_read_mode {
-        writeln!(stdout, "\r{}\r", format!("{:^1$}", "Choices".bold().italic().yellow(), game_state.terminal_width)).unwrap();
+        writeln!(stdout, "\r{}{}\r", color::Fg(color::Yellow), format!("{:^1$}", "Choices", game_state.terminal_width)).unwrap();
         for i in 0..story.option_text.len(){
             if i == story.selection_num{
-                write!(stdout, "\r{}\r", story.option_text[i].blue().bold()).unwrap();
+                write!(stdout, "\r{}{}\r", color::Fg(color::Blue), story.option_text[i]).unwrap();
             } else{
-                write!(stdout, "\r{}\r", story.option_text[i].bold()).unwrap();
+                write!(stdout, "\r{}{}\r", color::Fg(color::LightWhite), story.option_text[i]).unwrap();
             }
         }
     } else if game_state.re_read_mode {
-        writeln!(stdout, "\r{}\r", format!("{:^1$}", "Your Choice".bold().italic().yellow(), game_state.terminal_width)).unwrap();
+        writeln!(stdout, "\r{}{}\r", color::Fg(color::Yellow), format!("{:^1$}", "Your Choice", game_state.terminal_width)).unwrap();
         let choice_num: usize = game_state.story_path[game_state.previous_story_num].choice_num;
-        write!(stdout, "\r{}\r", story.option_text[choice_num].red().bold()).unwrap();
+        write!(stdout, "\r{}{}\r", color::Fg(color::Red), story.option_text[choice_num]).unwrap();
     }
 
-    writeln!(stdout, "\r\n{}\r", format!("{:^1$}", "To Quit, Press 'Esc'. For Help, Press 'h'".bold().italic().green(), game_state.terminal_width)).unwrap();
+    writeln!(stdout, "\r\n{}{}\r", color::Fg(color::Green), format!("{:^1$}", "To Quit, Press 'Esc'. For Help, Press 'h'", game_state.terminal_width)).unwrap();
     writeln!(stdout, "{}", Hide).unwrap();
     //print_story_status(&story, &game_state);
     stdout
@@ -238,31 +239,32 @@ fn write_story(story: &StoryPage, game_state: &GameState, mut stdout: Out) -> Ou
 // Writes the Help menu for the game
 fn help(game_state: &GameState, mut stdout: Out) -> Out{
     let width: usize = game_state.terminal_width;
-    let title: ColoredString =            " Help Menu ".bold().green();
-    let controls: ColoredString =         "Controls:".bold().yellow();
-    let header: ColoredString =           "Key           Action".bold().blue();
-    let enter_control: ColoredString =    "Enter         Continue the story with the selected option".italic();
-    let up_down_control: ColoredString =  "Up/Down       Select your choice".italic();
-    let re_read_control: ColoredString =  "Left/Right    Go back and forth through the story".italic();
-    let quit_control: ColoredString =     "Esc           Exit out of the game".italic();
-    let help_control: ColoredString =     "h             Open the help menu".italic();
-    let reset_control: ColoredString =    "r             Resets the game".italic();
-    let exit: ColoredString =             "  Press Any Key to return to the game  ".bold().green();
+    let title: String =           String::from("Help Menu");
+    let controls: String =        String::from("Controls:");
+    let header: String =          String::from("Key           Action");
+    let enter_control: String =   String::from("Enter         Continue the story with the selected option");
+    let up_down_control: String = String::from("Up/Down       Select your choice");
+    let re_read_control: String = String::from("Left/Right    Go back and forth through the story");
+    let quit_control: String =    String::from("Esc           Exit out of the game");
+    let help_control: String =    String::from("h             Open the help menu");
+    let reset_control: String =   String::from("r             Resets the game");
+    let exit: String =            String::from("Press Any Key to return to the game");
 
     // Printing the help screen
-    write!(stdout, "\r{}\r", "\x1bc").unwrap();
+    write!(stdout, "\r{}{}\r", "\x1bc", style::Bold).unwrap();
     writeln!(stdout, "").unwrap();
     writeln!(stdout, "").unwrap();
     writeln!(stdout, "").unwrap();
-    writeln!(stdout, "\r{}\r", format!("{:^1$}", title, width)).unwrap();
+    writeln!(stdout, "\r{}{}\r", color::Fg(color::Green), format!("{:^1$}", title, width)).unwrap();
+    writeln!(stdout, "{}{}", style::Italic, color::Fg(color::White)).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", "Welcome to Devolution, a sci-fi adventure where you choose", width)).unwrap();
+    writeln!(stdout, "\r{}\r", format!("{:^1$}", "how you progresses!", width)).unwrap();
     writeln!(stdout, "").unwrap();
-    writeln!(stdout, "\r{}\r", format!("{:^1$}", "Welcome to Devolution, a sci-fi adventure where you choose".italic(), width)).unwrap();
-    writeln!(stdout, "\r{}\r", format!("{:^1$}", "how you progresses!".italic(), width)).unwrap();
-    writeln!(stdout, "").unwrap();
-    writeln!(stdout, "").unwrap();
+    writeln!(stdout, "{}{}", style::NoItalic, color::Fg(color::Yellow)).unwrap();
     writeln!(stdout, "\r{}\r", format!("{:^1$}", controls, width)).unwrap();
     writeln!(stdout, "").unwrap();
-    writeln!(stdout, "\r{}\r", format!("{:>1$}", header, (width / 5) + header.len())).unwrap();
+    writeln!(stdout, "\r{}{}\r", color::Fg(color::Blue), format!("{:>1$}", header, (width / 5) + header.len())).unwrap();
+    writeln!(stdout, "{}{}", style::Italic, color::Fg(color::White)).unwrap();
     writeln!(stdout, "\r{}\r", format!("{:>1$}", enter_control, (width / 5) + enter_control.len())).unwrap();
     writeln!(stdout, "\r{}\r", format!("{:>1$}", up_down_control, (width / 5) + up_down_control.len())).unwrap();
     writeln!(stdout, "\r{}\r", format!("{:>1$}", re_read_control, (width / 5) + re_read_control.len())).unwrap();
@@ -271,7 +273,7 @@ fn help(game_state: &GameState, mut stdout: Out) -> Out{
     writeln!(stdout, "\r{}\r", format!("{:>1$}", reset_control, (width / 5) + reset_control.len())).unwrap();
     writeln!(stdout, "").unwrap();
     writeln!(stdout, "").unwrap();
-    writeln!(stdout, "\r{}\r", format!("{:^1$}", exit, width)).unwrap();
+    writeln!(stdout, "\r{}{}{}\r", style::NoItalic, color::Fg(color::Green), format!("{:^1$}", exit, width)).unwrap();
     writeln!(stdout, "\r").unwrap();
     writeln!(stdout, "{}", Hide).unwrap();
     stdout
@@ -309,7 +311,7 @@ fn write_character_creator(character: &Character, game_state: &GameState, mut st
             boy = format!("{}{}{}{}Boy", color::Fg(color::Blue), style::Blink, arrow, style::NoBlink);
             girl = format!("{} Girl", color::Fg(color::White));
         }
-        gender_option = format!(" {}{:>9}{}", boy,  " ", girl);
+        gender_option = format!(" {}{:>9}{}", boy, " ", girl);
     }
 
     // Printing the character creator screen
